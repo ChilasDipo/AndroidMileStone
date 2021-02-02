0bf7481783b87f64bf440620667fd59971961946
Chilas Main PC <Chil0041@edu.easj.dk>
2021-02-02 20:18:22 +0100

    Milepæl 2

--- app/src/main/java/com/example/myfirsttestingofandroid/MainActivity.java
+++ app/src/main/java/com/example/myfirsttestingofandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myfirsttestingofandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void buttonclick(View view){
+        TextView tv = (TextView)findViewById(R.id.textView);
+        TextView tvedit = (TextView)findViewById(R.id.editTextTextPersonName);
+        tv.setText(tvedit.getText());
     }
 }
