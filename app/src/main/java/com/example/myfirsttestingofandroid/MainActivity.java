bcc85131c072a9b7781c709af01cfa7feed06222
Chilas Main PC <Chil0041@edu.easj.dk>
2021-02-20 12:19:28 +0100

    Milepæl 3

--- app/src/main/java/com/example/myfirsttestingofandroid/MainActivity.java
+++ app/src/main/java/com/example/myfirsttestingofandroid/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import Logic.Controler;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Controler controler = new Controler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void buttonclick(View view){
+    public void SetLabelToTextFeild(View view){
         TextView tv = (TextView)findViewById(R.id.textView);
         TextView tvedit = (TextView)findViewById(R.id.editTextTextPersonName);
         tv.setText(tvedit.getText());
